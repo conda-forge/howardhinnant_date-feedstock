827d00b89dad793ae3a148eb9648494c4482fcd4
Julien Jerphanion <git@jjerphan.xyz>
2024-05-24 13:50:42 +0200

    Specify INSTALL on Windows
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,12 @@
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -D BUILD_SHARED_LIBS=ON -D BUILD_TZ_LIB=ON -D USE_SYSTEM_TZ_DB=ON -D CMAKE_BUILD_TYPE=Release
+cmake -G "NMake Makefiles" ^
+     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+     %SRC_DIR% ^
+     -D BUILD_SHARED_LIBS=ON ^
+     -D BUILD_TZ_LIB=ON ^
+     -D USE_SYSTEM_TZ_DB=ON ^
+     -D CMAKE_BUILD_TYPE=Release ^
+     -D INSTALL=%PREFIX%\share\zoneinfo\tzdata.zi
+
 if errorlevel 1 exit 1
 
 nmake
