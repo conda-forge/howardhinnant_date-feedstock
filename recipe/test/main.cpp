12619eb8dad2e02d2aa3619a8db5e05d28ca8072
Austin Morton <austin.morton@aquatic.com>
2024-10-17 14:52:15 +0000

    hopefully fixed now

--- recipe/test/main.cpp
+++ recipe/test/main.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 
 int main() {
-    auto const& tzdb = date::get_tzdb();
-    if (tzdb.locate_zone("CondaTest") == nullptr) {
+    if (date::locate_zone("CondaTest") == nullptr) {
         std::cout << "CondaTest zone not found, something is wrong with the patch\n";
         return 1;
     }
-    std::cout << "num zones: " << tzdb.zones.size() << "\n";
     return 0;
 }
