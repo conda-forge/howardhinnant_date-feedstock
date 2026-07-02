6f7f80f018c94211a83815c0f7ad922dabc492bd
Julien Jerphanion <git@jjerphan.xyz>
2026-07-02 10:07:58 +0200

    Adapt `howardhinnant_date-static` tests and `run_exports`
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/test/run_static_tests.bat
+++ recipe/test/run_static_tests.bat
@@ -5,11 +5,6 @@ cd /d %~dp0
 python check_package_libs.py static
 if errorlevel 1 exit 1
 
-for /f "delims=" %%f in ('dir /b "%SRC_DIR%\..\..\linux-64\howardhinnant_date-*.conda" ^| findstr /v static') do set HOWARDHINNANT_DATE_SH_PKG=%SRC_DIR%\..\..\linux-64\%%f
-if not defined HOWARDHINNANT_DATE_SH_PKG exit 1
-conda install --no-deps -y -p "%PREFIX%" "%HOWARDHINNANT_DATE_SH_PKG%"
-if errorlevel 1 exit 1
-
 if not exist "%LIBRARY_PREFIX%\include\date\date.h" exit 1
 if not exist "%LIBRARY_PREFIX%\cmake\dateConfig.cmake" exit 1
 
--- recipe/test/run_static_tests.sh
+++ recipe/test/run_static_tests.sh
@@ -6,10 +6,6 @@ cd "${script_dir}"
 
 python check_package_libs.py static
 
-howardhinnant_date_sh_pkg="$(ls "$(dirname "$(dirname "${PREFIX}")")/linux-64"/howardhinnant_date-*.conda | grep -v static | head -1)"
-test -n "${howardhinnant_date_sh_pkg}"
-conda install --no-deps -y -p "${PREFIX}" "${howardhinnant_date_sh_pkg}"
-
 test -d "${PREFIX}/include/date"
 test -f "${PREFIX}/include/date/date.h"
 test -f "${PREFIX}/lib/cmake/date/dateConfig.cmake"
