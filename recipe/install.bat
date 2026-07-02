d3bb181d26b453078d0d1bc7f6e0e5f20626afdc
Julien Jerphanion <git@jjerphan.xyz>
2026-07-02 11:31:23 +0200

    Do not build `howardhinnant_date-static` for Windows
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/install.bat
+++ recipe/install.bat
@@ -1,28 +1,5 @@
 @echo on
 
-echo %PKG_NAME% | findstr /I /C:"static" >nul
-if not errorlevel 1 goto :build_static
-goto :build_shared
-
-:build_static
-"%BUILD_PREFIX%\bin\cmake" -S . -B build-static %CMAKE_ARGS% -G "NMake Makefiles" ^
-  -DBUILD_SHARED_LIBS=OFF ^
-  -DUSE_SYSTEM_TZ_DB=ON ^
-  -DBUILD_TZ_LIB=ON ^
-  -DCMAKE_BUILD_TYPE=Release ^
-  -DCMAKE_RELEASE_POSTFIX="_static" ^
-  -DCMAKE_CXX_STANDARD=17 ^
-  -DCMAKE_CXX_STANDARD_REQUIRED=TRUE
-if errorlevel 1 exit /b 1
-
-"%BUILD_PREFIX%\bin\cmake" --build build-static --config Release
-if errorlevel 1 exit /b 1
-
-"%BUILD_PREFIX%\bin\cmake" --install build-static --config Release
-if errorlevel 1 exit /b 1
-goto :eof
-
-:build_shared
 "%BUILD_PREFIX%\bin\cmake" -S . -B build-dyn %CMAKE_ARGS% -G "NMake Makefiles" ^
   -DBUILD_SHARED_LIBS=ON ^
   -DUSE_SYSTEM_TZ_DB=ON ^
@@ -37,4 +14,3 @@ if errorlevel 1 exit /b 1
 
 "%BUILD_PREFIX%\bin\cmake" --install build-dyn --config Release
 if errorlevel 1 exit /b 1
-goto :eof
