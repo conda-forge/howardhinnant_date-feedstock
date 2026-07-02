bf69d64674a86a6e4ff99efcfb53a6ee71c2289d
Julien Jerphanion <git@jjerphan.xyz>
2026-07-02 11:21:38 +0200

    Use labels for bat script
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/install.bat
+++ recipe/install.bat
@@ -1,34 +1,40 @@
 @echo on
 
-IF not "x%PKG_NAME:static=%" == "x%PKG_NAME%" (
-    "%BUILD_PREFIX%\bin\cmake" -S . -B build-static %CMAKE_ARGS% -G "NMake Makefiles" ^
-      -DBUILD_SHARED_LIBS=OFF ^
-      -DUSE_SYSTEM_TZ_DB=ON ^
-      -DBUILD_TZ_LIB=ON ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_RELEASE_POSTFIX="_static" ^
-      -DCMAKE_CXX_STANDARD=17 ^
-      -DCMAKE_CXX_STANDARD_REQUIRED=TRUE
-    if errorlevel 1 exit 1
+echo %PKG_NAME% | findstr /I /C:"static" >nul
+if not errorlevel 1 goto :build_static
+goto :build_shared
 
-    "%BUILD_PREFIX%\bin\cmake" --build build-static --config Release
-    if errorlevel 1 exit 1
+:build_static
+"%BUILD_PREFIX%\bin\cmake" -S . -B build-static %CMAKE_ARGS% -G "NMake Makefiles" ^
+  -DBUILD_SHARED_LIBS=OFF ^
+  -DUSE_SYSTEM_TZ_DB=ON ^
+  -DBUILD_TZ_LIB=ON ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_RELEASE_POSTFIX="_static" ^
+  -DCMAKE_CXX_STANDARD=17 ^
+  -DCMAKE_CXX_STANDARD_REQUIRED=TRUE
+if errorlevel 1 exit /b 1
 
-    "%BUILD_PREFIX%\bin\cmake" --install build-static --config Release
-    if errorlevel 1 exit 1
-) ELSE (
-    "%BUILD_PREFIX%\bin\cmake" -S . -B build-dyn %CMAKE_ARGS% -G "NMake Makefiles" ^
-      -DBUILD_SHARED_LIBS=ON ^
-      -DUSE_SYSTEM_TZ_DB=ON ^
-      -DBUILD_TZ_LIB=ON ^
-      -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_CXX_STANDARD=17 ^
-      -DCMAKE_CXX_STANDARD_REQUIRED=TRUE
-    if errorlevel 1 exit 1
+"%BUILD_PREFIX%\bin\cmake" --build build-static --config Release
+if errorlevel 1 exit /b 1
 
-    "%BUILD_PREFIX%\bin\cmake" --build build-dyn --config Release
-    if errorlevel 1 exit 1
+"%BUILD_PREFIX%\bin\cmake" --install build-static --config Release
+if errorlevel 1 exit /b 1
+goto :eof
 
-    "%BUILD_PREFIX%\bin\cmake" --install build-dyn --config Release
-    if errorlevel 1 exit 1
-)
+:build_shared
+"%BUILD_PREFIX%\bin\cmake" -S . -B build-dyn %CMAKE_ARGS% -G "NMake Makefiles" ^
+  -DBUILD_SHARED_LIBS=ON ^
+  -DUSE_SYSTEM_TZ_DB=ON ^
+  -DBUILD_TZ_LIB=ON ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_CXX_STANDARD=17 ^
+  -DCMAKE_CXX_STANDARD_REQUIRED=TRUE
+if errorlevel 1 exit /b 1
+
+"%BUILD_PREFIX%\bin\cmake" --build build-dyn --config Release
+if errorlevel 1 exit /b 1
+
+"%BUILD_PREFIX%\bin\cmake" --install build-dyn --config Release
+if errorlevel 1 exit /b 1
+goto :eof
