cd94b83f526443fab2594e81eeb3d75721bb8f12
Julien Jerphanion <git@jjerphan.xyz>
2026-07-02 11:48:55 +0200

    Revert changes made to bld.bat
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,14 @@
-@echo on
+echo on
 
-"%BUILD_PREFIX%\bin\cmake" -S . -B build-dyn %CMAKE_ARGS% -G "NMake Makefiles" ^
+cmake -S . -B build %CMAKE_ARGS% -G "NMake Makefiles" ^
   -DBUILD_SHARED_LIBS=ON ^
   -DUSE_SYSTEM_TZ_DB=ON ^
   -DBUILD_TZ_LIB=ON ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_CXX_STANDARD=17 ^
-  -DCMAKE_CXX_STANDARD_REQUIRED=TRUE
-if errorlevel 1 exit /b 1
+  -DCMAKE_CXX_STANDARD_REQUIRED=TRUE ^
 
-"%BUILD_PREFIX%\bin\cmake" --build build-dyn --config Release
-if errorlevel 1 exit /b 1
+if errorlevel 1 exit 1
 
-"%BUILD_PREFIX%\bin\cmake" --install build-dyn --config Release
-if errorlevel 1 exit /b 1
+cmake --build build --config Release --target install
+if errorlevel 1 exit 1
